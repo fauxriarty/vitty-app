137a0cc270d4c83c4531b3689bac64a50fdda5e0
rudrankbasant <rudrank123@gmail.com>
2023-09-11 17:53:15 +0530

    feat: hide bottom nav in sub fragments

--- app/src/main/java/com/dscvit/vitty/activity/HomeActivity.kt
+++ app/src/main/java/com/dscvit/vitty/activity/HomeActivity.kt
@@ -1,6 +1,7 @@
 package com.dscvit.vitty.activity
 
 import android.os.Bundle
+import android.view.View
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.FragmentActivity
 import androidx.navigation.findNavController
@@ -22,6 +23,17 @@ class HomeActivity : FragmentActivity() {
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
 
         navView.setupWithNavController(navController)
+
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if(destination.id == R.id.friendFragment || destination.id == R.id.searchFragment || destination.id == R.id.navigation_requests) {
+
+                binding.navView.visibility = View.GONE
+            } else {
+
+                binding.navView.visibility = View.VISIBLE
+            }
+        }
     }
 
 
