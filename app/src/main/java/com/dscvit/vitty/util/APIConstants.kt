652889a00c558266d5e07c628a1a2036ce38bc53
illuminadi <aditya.chheda04@gmail.com>
2024-08-06 00:26:16 +0530

    initial commit

--- app/src/main/java/com/dscvit/vitty/util/APIConstants.kt
+++ app/src/main/java/com/dscvit/vitty/util/APIConstants.kt
@@ -2,5 +2,6 @@ package com.dscvit.vitty.util
 
 object APIConstants {
     const val BASE_URL = "https://shielded-beyond-17845.herokuapp.com"
-    const val TIMEOUT = 10
+    const val TIMEOUT = 100
+    const val Community_BASE_URL = "https://vitty-api.dscvit.com"
 }
